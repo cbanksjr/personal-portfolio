d3f3b1e42ce5f7094a233e6b6b83b7d1277ab948
cbanksjr <craigbjr241@gmail.com>
2023-12-11 12:42:03 -0500

    add: blank target to anchor tag

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -12,8 +12,8 @@ const Nav = ({ link1, link2 }) => {
    
       <nav className="lg:flex justify-end p-8">
         <ul className="flex flex-row justify-center lg:flex mt-4 text-xl">
-          <li className="pr-2 lg:mr-20 cursor-pointer text-white hover:text-[#f66b00] duration-300 hover:duration-500"><a href={link1}>{linkedIn}</a></li>
-          <li className="pl-2 lg:mr-36 text-white hover:text-[#f66b00] duration-500 hover:duration-300"><a href={link2}>{gitHub}</a></li>
+          <li className="pr-2 lg:mr-20 cursor-pointer text-white hover:text-[#f66b00] duration-300 hover:duration-500"><a href={link1} target="_blank" rel="noreferrer">{linkedIn}</a></li>
+          <li className="pl-2 lg:mr-36 text-white hover:text-[#f66b00] duration-500 hover:duration-300"><a href={link2} target="_blank" rel="noreferrer">{gitHub}</a></li>
         </ul>
       </nav>
   
